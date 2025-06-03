dd1433bbbe23819ccae1fd56c6fb57bbb9dc944a
AndersonCh-96 <andersonxavier8@hotmail.com>
2025-06-02 21:33:19 -0500

    add

--- src/components/react/Swiper.jsx
+++ src/components/react/Swiper.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
+
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-coverflow";
+import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 import "./styles.css";
 ("use client");
 
-import { EffectCoverflow, Pagination } from "swiper/modules";
 import { useCart } from "../../store/cartProduct";
 import toast from "react-hot-toast";
+import { FreeMode, Pagination } from "swiper/modules";
 
 export default function SwiperCarousel({ products, addProduct }) {
   const [product, setProduct] = useState({});
@@ -46,28 +49,22 @@ export default function SwiperCarousel({ products, addProduct }) {
   return (
     <div>
       <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        //   pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper w-full"
+        slidesPerView={4}
+        spaceBetween={30}
+        freeMode={true}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
       >
         {products.map((product) => (
-          <SwiperSlide key={product.id} className="w-full -">
-            <div className="bg-white p-4 shadow-2xl w-full hover:scale-110 transition-transform ">
+          <SwiperSlide key={product.id} className="">
+            <div className="bg-white  py-3 shadow-2xl rounded-2xl hover:scale-110 transition-transform ">
               <img
                 src={product.image}
                 alt={product.title}
-                className="w-full h-48 object-cover rounded-lg"
+                className="size-40 object-contain"
               />
               <h3 className="mt-2 text-center">{product.title}</h3>
 
@@ -88,7 +85,7 @@ export default function SwiperCarousel({ products, addProduct }) {
                   data-modal-toggle="react-modal"
                   className="bg-blue-500 text-white px-4 py-2 rounded-2xl cursor-pointer"
                 >
-                  Abrir Modal
+                  Agregar
                 </button>
               </div>
             </div>
@@ -96,64 +93,65 @@ export default function SwiperCarousel({ products, addProduct }) {
         ))}
       </Swiper>
 
-      <div>
-        <div
-          id="react-modal"
-          tabIndex="-1"
-          className="hidden fixed inset-0 z-50 overflow-y-auto "
-        >
-          <div className="relative p-4 w-full max-w-3xl mx-auto my-8 transition-transform">
-            <div className="bg-white flex flex-col justify-between rounded-lg shadow p-6  ">
-              <div className="flex justify-between">
-                <h3 className="text-xl font-bold mb-4 line-clamp-2 w-full ">
-                  {product.title}
-                </h3>
+      <div
+        id="react-modal"
+        aria-hidden="true"
+        tabIndex="-1"
+        className="hidden fixed inset-0 z-50 overflow-y-auto "
+      >
+        <div className="relative p-4 w-full max-w-3xl mx-auto my-8 transition-transform">
+          <div className="bg-white flex flex-col justify-between rounded-lg shadow p-6  ">
+            <div className="flex justify-between">
+              <h3 className="text-xl font-bold mb-4 line-clamp-2 w-full ">
+                {product?.title}
+              </h3>
 
+              <button
+                data-modal-hide="react-modal"
+                className="bg-red-600 px-4 w-12 h-10 rounded-xl cursor-pointer text-white"
+              >
+                X
+              </button>
+            </div>
+            <div className="grid grid-cols-2">
+              <div>
+                <img
+                  src={product?.image}
+                  alt={product?.title}
+                  className="w-full h-48 object-contain justify-center items-center place-content-center rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col">
+                <p>{product?.description}</p>
+                <p>${product?.price}</p>
+              </div>
+            </div>
+
+            <div className="flex justify-between ">
+              <div className="flex gap-6 justify-center items-center">
                 <button
-                  data-modal-hide="react-modal"
-                  className="bg-red-600 px-4 w-12 h-10 rounded-xl cursor-pointer text-white"
+                  onClick={decrementQuantity}
+                  className="bg-blue-700 text-white  text-lg px-4 py-1 rounded cursor-pointer"
                 >
-                  X
+                  -
+                </button>
+                <p>{quantity}</p>
+                <button
+                  onClick={incrementQuantity}
+                  className="bg-blue-700 text-white text-lg  px-4 py-1 rounded cursor-pointer"
+                >
+                  +
                 </button>
               </div>
-              <div className="grid grid-cols-2">
-                <div>
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    className="w-full h-48 object-contain justify-center items-center place-content-center rounded-lg"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <p>{product.description}</p>
-                  <p>${product.price}</p>
-                </div>
-              </div>
-
-              <div className="flex justify-between ">
-                <div className="flex gap-6 justify-center items-center">
-                  <button
-                    onClick={decrementQuantity}
-                    className="bg-blue-700 text-white  text-lg px-4 py-1 rounded cursor-pointer"
-                  >
-                    -
-                  </button>
-                  <p>{quantity}</p>
-                  <button
-                    onClick={incrementQuantity}
-                    className="bg-blue-700 text-white text-lg  px-4 py-1 rounded cursor-pointer"
-                  >
-                    +
-                  </button>
-                </div>
-                <button
-                  // onClick={() => addProduct(product)}
-                  onClick={() => handleAddToCart(product)}
-                  className="bg-green-500 cursor-pointer text-white px-6 rounded-2xl py-2 text-center"
-                >
-                  Agregar
-                </button>
-              </div>
+              <button
+                // data-modal-hide="react-modal"
+                onClick={() => {
+                  handleAddToCart(product);
+                }}
+                className="bg-green-500 cursor-pointer text-white px-6 rounded-2xl py-2 text-center"
+              >
+                Agregar
+              </button>
             </div>
           </div>
         </div>
@@ -161,3 +159,6 @@ export default function SwiperCarousel({ products, addProduct }) {
     </div>
   );
 }
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
